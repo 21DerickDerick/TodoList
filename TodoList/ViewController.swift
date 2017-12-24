42f05bc9ce81d607ec1e4e9889fb7ab90f15ec05
Derick Derick <DerickDerick@Dericks-MacBook-Air.local>
2017-12-24 09:50:16 +0800

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Derick Derick on 24/12/2017.
+//  Copyright © 2017 indiemax. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
